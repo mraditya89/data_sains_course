abc623913718dfa75546ab4dc0198a6d0a2f4558
mraditya89 <yudi.aditya.89@gmail.com>
2021-12-03 10:38:39 +0700

    updated assignment 24

---
+++ sandbox/w6d5_practice_3.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup 
+import requests
+from requests.sessions import Request
+
+
+class PRCrawl:
+    __baseUrl = "https://www.pikiran-rakyat.com"
+    __pageLimit = 10        
+
+    def __init__(self, category, numPage=1):
+        self.__url = f"{PRCrawl.__baseUrl}/{category}"
+        self.__numPage = numPage if numPage<= PRCrawl.__pageLimit else PRCrawl.__pageLimit
+        self.__getTitleList()
+        self.__mainDocs = []
+    
+    def __getTitleList(self):
+        for idx in range(self.__numPage):
+            page = idx+1
+            response = requests.get(f"{self.__url}?page={page}" )
+            self.__mainDocs.append(
+                BeautifulSoup(response.text, 'html.parser')
+            )
+            
+
+
+            
+
+
+
+
+        # response = requests.get(url)
+        # self.__doc = BeautifulSoup(response.text, 'html.parser')
+    
+
+doc = PRCrawl("nasional", numPage=2)
+
